d9d9ff52d2d31153d2e28e2df9f153ec108dc34b
vajradhar13 <dvajradhar@gmail.com>
2026-02-26 20:12:46 +0530

    added the ci pipeline

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,18 @@ app.use(express.json());
 app.post("/sum", (req, res) => {
   const a = req.body.a;
   const b = req.body.b;
-  const answer = a - b;
+  const answer = a + b;
 
   res.json({
     answer,
   });
+});
+
+// 👇 THIS IS WHAT YOU'RE MISSING
+const PORT = 3000;
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
